bc339572cd9b1b3da6016b35f233133d30cdafe8
Leonel Medina <ldavid.mdn@gmail.com>
2023-12-22 19:09:22 -0500

    Finalizacion de clase 7'

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import './TodoCounter.css';
+
+function TodoCounter({ total, complete }) {
+    return (
+        <h1 className="TodoCounter">
+            Has completado <span>{complete}</span> de <span>{total}</span> TODOs
+        </h1>
+    )
+}
+
+export { TodoCounter };
