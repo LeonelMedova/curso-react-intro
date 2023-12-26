e27d570bdfc0084d5bc4f7141c1c90bdabd4a013
Leonel Medina <ldavid.mdn@gmail.com>
2023-12-26 15:32:43 -0500

    Finalización de la clase 9 con busqueda de TODOs en mayúscula y minúscula sin problema

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ function App() {
     const completedTodos = todos.filter(todo => !!todo.completed).length;
     const totalTodos = todos.length;
 
+    const searchedTodos = todos.filter(
+      todo => {
+        const todoText = todo.text.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+
+        return todoText.includes(searchText);
+      }
+    );
+
     console.log('Los usuarios buscan ToDos de ' + searchValue);
     console.log(completedTodos);
 
@@ -36,7 +45,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text}
             text={todo.text}
