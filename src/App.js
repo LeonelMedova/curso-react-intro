21f09df7eaad08f7ae76cc9b5bef5e11c720a8ee
Leonel Medina <ldavid.mdn@gmail.com>
2023-12-27 17:40:19 -0500

    Clase 14 finalizada con Custom Hooks useLocalStorage

--- src/App.js
+++ src/App.js
@@ -16,62 +16,67 @@ import { CreateTodoButton } from './CreateTodoButton';
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
 
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  
+  let parsedItem;
+  
+  if(!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem)
+  }
+  
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, saveItem]
+}
+
 function App() {
-    const localStorageTodos = localStorage.getItem('TODOS_V1');
-
-    let parsedTodos;
-
-    if(!localStorageTodos) {
-      localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    } else {
-      parsedTodos = JSON.parse(localStorageTodos)
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
+  const [searchValue, setSearchValue] = React.useState ('');
+  
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  
+  const searchedTodos = todos.filter(
+    todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      
+      return todoText.includes(searchText);
     }
-
-    const [todos, setTodos] = React.useState(parsedTodos)
-    const [searchValue, setSearchValue] = React.useState ('');
-
-    const completedTodos = todos.filter(todo => !!todo.completed).length;
-    const totalTodos = todos.length;
-
-    const searchedTodos = todos.filter(
-      todo => {
-        const todoText = todo.text.toLowerCase();
-        const searchText = searchValue.toLowerCase();
-
-        return todoText.includes(searchText);
-      }
+  );
+  
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
     );
-
-    const saveTodos = (newTodos) => {
-      localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-
-      setTodos(newTodos);
-    };
-    
-    const completeTodo = (text) => {
-      const newTodos = [...todos];
-      const todoIndex = newTodos.findIndex(
-        (todo) => todo.text === text
-      );
-      newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-      saveTodos(newTodos);
-    };
-
-    const deleteTodo = (text) => {
-      const newTodos = [...todos];
-      const todoIndex = newTodos.findIndex(
-        (todo) => todo.text === text
-      );
-      newTodos.splice(todoIndex, 1);
-      saveTodos(newTodos);
-    };
-
-
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    saveTodos(newTodos);
+  };
+  
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
     console.log(completedTodos);
-
+    
   return (
     <>
-
+    
       <TodoCounter 
         completed={completedTodos} 
         total={totalTodos} 
@@ -80,7 +85,7 @@ function App() {
         searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
-
+      
       <TodoList>
         {searchedTodos.map(todo => (
           <TodoItem 
@@ -92,7 +97,7 @@ function App() {
           />
         ))}
       </TodoList>
-
+      
       <CreateTodoButton/>
     </>
   );
