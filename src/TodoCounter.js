4bb76446a067644c74793193934be469714f3e2e
Leonel Medina <ldavid.mdn@gmail.com>
2023-12-27 08:58:59 -0500

    Finalización del reto de la clase 10 con props en bold

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+    const todoCounterNormal = <>Has completado <span>{completed}</span> de <span>{total}</span> TODOs</>
+
     return (
         <h1 className="TodoCounter">{
             total === completed 
-            ? `Felicitaciones, terminaste todos los TODOs` 
-            : `Has completado ${completed} de ${total} TODOs`
+            ? `🎆🎊🎉Felicitaciones, terminaste todos los TODOs!🎉🎊🎆` 
+            : todoCounterNormal
         
         }</h1>
     )
