f8aa760cc9c0d27093c3a8e545251a11e13e8a75
Leonel Medina <ldavid.mdn@gmail.com>
2024-01-24 17:12:11 -0500

    Finalización clase 22 con problemas en el TodoCounter y TodoSearch para solucionar en sig clase

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -45,3 +45,16 @@ function useLocalStorage(itemName, initialValue) {
 }
 
 export { useLocalStorage };
+
+
+// localStorage.removeItem('TODOS_V1');
+
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el curso de React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'Cuidar a Leito', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ]
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
