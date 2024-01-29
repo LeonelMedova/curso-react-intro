def468487933120dae8e6d6226513bb7747e9a8d
Leonel Medina <ldavid.mdn@gmail.com>
2024-01-29 08:38:40 -0500

    Finalización clase 27 con la app y sus funcionalidades listas

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -11,7 +11,7 @@ function TodoProvider({ children }) {
         error
     } = useLocalStorage('TODOS_V1', [])
     const [searchValue, setSearchValue] = React.useState ('');
-    const [openModal, setOpenModal] = React.useState (true);
+    const [openModal, setOpenModal] = React.useState (false);
     
     const completedTodos = todos.filter(todo => !!todo.completed).length;
     const totalTodos = todos.length;
@@ -33,7 +33,14 @@ function TodoProvider({ children }) {
         newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
         saveTodos(newTodos);
     };
-    
+    const addTodo = (text) => {
+        const newTodos = [...todos];
+        newTodos.push({
+            text,
+            completed: false,
+        })
+        saveTodos(newTodos);
+    }
     const deleteTodo = (text) => {
         const newTodos = [...todos];
         const todoIndex = newTodos.findIndex(
@@ -52,6 +59,7 @@ function TodoProvider({ children }) {
             searchValue,
             setSearchValue,
             searchedTodos,
+            addTodo,
             completeTodo,
             deleteTodo,
             openModal,
